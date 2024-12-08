b89a9c2b95488a7c3aa24acb13f562eb771bd374
Edesildo Wanderley <edesildo@gmail.com>
2024-12-08 18:08:58 -0300

    Exercise study question 2

---
+++ exercicio/ex02.js
@@ -0,0 +1,9 @@
+//Crie duas variáveis, uma contendo seu primeiro nome e outra contendo seu último nome. Em seguida, combine-as em uma terceira variável usando o operador + 
+//e em uma quarta variável usando template strings. Por fim, imprima os resultados obtidos no console.
+
+let nome = 'Edesildo';
+let sobrenome = 'Santos';
+let cidade = 'Recife';
+let idade = 25;
+
+console.log('Meu nome é ' + nome + ' e meu sobrenome é ' + sobrenome + '.Minha cidade é ' + cidade +` tenho ${idade} de idade`);
