1d3f0783b45b7157386f3ce091e9c6a437172646
Edesildo Wanderley <edesildo@gmail.com>
2024-12-08 18:24:21 -0300

    question 4 exercise

---
+++ exercicio/ex04.js
@@ -0,0 +1,9 @@
+//Crie uma variável inicializada com um valor de qualquer tipo e, em seguida, reatribua um novo valor a essa variável. Imprima ambos os valores no console após atribuí-los.
+
+let valor = 34;
+
+console.log("Valor inicial: " + valor)
+
+valor = "Um belo dia"
+
+console.log("Valor atribuído : "+ valor)
