79b30c4253ed1a483d0ee05775639bfad0e0c7ec
Edesildo Wanderley <edesildo@gmail.com>
2024-12-08 18:21:02 -0300

    question 3 exercise

---
+++ exercicio/ex03.js
@@ -0,0 +1,9 @@
+//Declare duas variáveis, uma contendo um número e outra contendo uma string. 
+//Em seguida, combine-as em uma terceira variável usando template strings para montar uma frase e exiba o resultado no console.
+
+let idade = 25;
+let nome = 'Edesildo'
+
+let mensagem = `Olá, meu nome é ${nome} tenho ${idade} anos`
+
+console.log(mensagem)
