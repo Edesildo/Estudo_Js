2a98d776341334396ed71f7ed43f377c6c7eeca9
Edesildo Wanderley <edesildo@gmail.com>
2024-12-08 19:31:06 -0300

    quetion 06 exercise

---
+++ exercicio/ex06.js
@@ -0,0 +1,7 @@
+let chovendo = false;
+
+if (chovendo) {
+    console.log("Está chovendo. É preciso levar um guarda-chuva.");
+} else {
+    console.log("Não está chovendo. Não é preciso levar um guarda-chuva.");
+}
